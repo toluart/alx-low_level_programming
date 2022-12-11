f20d932a5a388871bffdf69fea214ada37633366
Tolu Araba <tolu.araba@gmail.com>
2022-12-11 12:56:57 -0800

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-main text
-return o
+* main - Entry
+* Return:Always 0
 **/
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - entry - text
- * return o
+ * main - Entry
+ * Return: Always 0
 **/
 int main(void)
 {

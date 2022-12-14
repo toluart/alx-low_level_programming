275d1ffc4316ce96cc1d69b30beb734319eeb4dc
Tolu Araba <tolu.araba@gmail.com>
2022-12-13 20:17:28 -0800

    2-print_alphabet_x10.c 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
-#include 'main.h'
+#include "main.h"
 
 /**
- * print alphabet x10 - entry point
+ * main - alphabet
  *
  * print_alphabet_x10 - print a - z ten times
  **/
 
-void print_alphabetx10(void)
+void print_alphabet_x10(void)
 {
 	int i;
 	char j;
 
-	for (i = 0; i < 10, i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{

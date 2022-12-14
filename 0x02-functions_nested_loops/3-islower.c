7f947025a9578709de336b2bdc6af6b181726ba5
Tolu Araba <tolu.araba@gmail.com>
2022-12-13 20:23:36 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: 0 Always.
+ *
+ **/
+
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1)
+	}
+	else
+	{
+		return (0);
+	}

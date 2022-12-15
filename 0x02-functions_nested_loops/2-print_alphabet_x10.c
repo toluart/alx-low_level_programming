aa04445660b392efbf71c2cbd18c65284caed4b3
Tolu Araba <tolu.araba@gmail.com>
2022-12-15 05:28:51 -0800

    second 23

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,9 @@
 /**
  * main - Entry point
  *
- * Description: print alphabet 10x
+ * Description print_alphabet_x10: print alphabet 10x
+ *
+ * Return: Always 0.
  **/
 
 void print_alphabet_x10(void)

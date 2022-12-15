1cdcab5d1fa1d891f053fc9656a6c57dd997e656
Tolu Araba <tolu.araba@gmail.com>
2022-12-15 06:17:21 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,8 @@
 
 /**
  * print_sign - returns 0 letter not lowercase, 1 letter lowercase
- *
  * @n: number to check
- * Return: 0 or 1
+ * Return: 0, 1 or -1
  */
 int print_sign(int n)
 {
@@ -12,16 +11,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1)
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		return (0)
+		return (0);
 	}
 	else
 		_putchar('-');
-		return (-1)
+		return (-1);
 	}
 	return (0);
 }

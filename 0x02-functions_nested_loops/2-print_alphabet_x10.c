f2d3a89b987be70f1ae265665395035a2af997de
Tolu Araba <tolu.araba@gmail.com>
2022-12-16 09:17:24 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Description print_alphabet_x10: print alphabet 10x
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
  *
  * Return: x10 a-z
  **/
